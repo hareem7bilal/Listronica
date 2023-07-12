64f06fa36ad23af8062ac7130a070d67dfcfc080
root <root@DESKTOP-LGO829B>
2023-07-12 00:18:07 -0700

    initial commit

---
+++ src/webdev/core.clj
@@ -0,0 +1,31 @@
+(ns webdev.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
+
+(defn greet[req]
+  {
+   :status 200
+   :body "hellooo"
+   :headers {}
+  })
+
+(defn goodbye [req]
+  {:status 200
+   :body "bye"
+   :headers {}})
+
+
+(defroutes app
+  (GET "/" [] greet) 
+  (GET "/goodbye" [] goodbye)
+  (not-found "page not found"))
+
+(defn -main [port]
+  (jetty/run-jetty app
+                   {:port (Integer. port)}))
+
+(defn -dev-main [port]
+  (jetty/run-jetty (wrap-reload #'app)
+                   {:port (Integer. port)}))
